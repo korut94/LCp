94f4eb57b3ffe4a0c56c24478fa72a0475064acb
korut94 <andreamnt94@gmail.com>
2014-07-26 17:29:36 +0200

    Il costruttore non salva puntatori ma crea una nuovo istanza di Predicate

--- src/lcputility/Derivate.java
+++ src/lcputility/Derivate.java
@@ -1,6 +1,5 @@
 package lcputility;
 
-@SuppressWarnings( "unused" )
 public class Derivate 
 {
 	private Predicate sequent;
@@ -10,9 +9,27 @@ public class Derivate
 	 * @param a : sequente da derivare
 	 * @param b : regola da utilizzare o assioma usato in caso di una foglia
 	 */
-	public Derivate( Predicate a, String b )
+	public Derivate( Predicate a )
 	{
-		sequent = a;
-		rule = b;
+		sequent = new Predicate( a.prSx, a.prDx );
+	}
+	
+	
+	/**
+	 * @return Stampa a video la regola affiancata dal sequente
+	 */
+	public void print()
+	{
+		System.out.print( rule + " " );
+		sequent.printPredicate();
+	}
+	
+	
+	/**
+	 * @param ruleUse : setta come regola utilizzata per derivare il sequente il contenuto di <i>ruleUse</i>
+	 */
+	public void setRule( String ruleUse )
+	{
+		rule = ruleUse;
 	}
 }
