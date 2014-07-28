37b14be8cb0bb7b816decf2e93966f6bd2e0dca4
korut94 <andreamnt94@gmail.com>
2014-07-28 17:43:46 +0200

    Il contenuto dell'albero e' leggibile e stampabile a video

--- src/lcputility/Solve.java
+++ src/lcputility/Solve.java
@@ -7,6 +7,15 @@ import java.util.Stack;
 /**
  * @author Mantovani Andrea
  * 
+ * @version 1.3.2
+ * - Il contenuto dell'albero sono sequenti leggibili e stampabili su schermo
+ * 
+ * - DA AGGIUNGERE NELLE VERSIONI SUCCESSIVE:
+ * 	 + Provare a sostituire StringBuilder al posto delle stringhe per vedere se aumenta l'efficienza
+ * 	 + Controllo sintattico del sequente
+ * 	 + Il sequente ed il suo negato vengono derivati in parallelo
+ * 
+ * 
  * @version 1.3.1
  * - L'albero di derivazione e' inserito dentro ad una struttura ad albero binario restituito dalla funzione treeLeaf
  * - L'ArrayList <i>sequents</i> viene svuotato al momento della derivazione del sequente negato
@@ -87,11 +96,11 @@ public class Solve implements Runnable
         //Negato del sequente
         String negative = "-(";
         
-        if( sx.isEmpty() ) negative += "(%)>";
-        else negative += '(' + sx + ")>";
+        if( sx.isEmpty() ) negative += "[%]>";
+        else negative += '[' + sx + "]>";
         
-        if( dx.isEmpty() ) negative += "(@))";
-        else negative += '(' + dx + "))";
+        if( dx.isEmpty() ) negative += "[@])";
+        else negative += '[' + dx + "])";
         
         negative = compatta( negative ) + ",@";
 
@@ -117,6 +126,8 @@ public class Solve implements Runnable
         negSequent = new Predicate( Arrays.asList( elemNeSx ), Arrays.asList( elemNeDx ) );
 
         reader = true;
+        
+        tableGroup.printAllReference();
     }
 
 
@@ -124,8 +135,7 @@ public class Solve implements Runnable
     public Tree treeLeaf()
     {   
     	//Creo la radice dell'albero di derivazione
-    	alberoDerivato.addSx( sequents.get( 0 ), 0 );
-    	
+    	attachTreeSx( sequents.get( 0 ).prSx, sequents.get( 0 ).prDx, 0 );
     	//Deriva l'albero utilizzando un thread per ramo
         run();
         
@@ -137,7 +147,8 @@ public class Solve implements Runnable
         
         //Continuo dal nodo radice
         alberoDerivato.reset();
-        alberoDerivato.addDx( negSequent, 0 );
+        //Attacco il sequente negato sul ramo destro della radice
+        attachTreeDx( sequents.get( 0 ).prSx, sequents.get( 0 ).prDx, 0 );
         
         run();
        
@@ -199,22 +210,27 @@ public class Solve implements Runnable
 
 
 
-    private CompactInfo saveGroup( int start, String pr )
+    private CompactInfo saveGroup( int start, String pr, boolean bracketObligatory )
     {
-        String compactPr = new String();
+    	boolean addBracket = false;
+    	
+    	int index = start;
+    	
+        String compactPr = new String(); 
+        
 
-        int index = start;
-
-        while( pr.charAt( index ) != ')' )
+        while( pr.charAt( index ) != ')' && pr.charAt( index ) != ']' )
         {
             char c = pr.charAt( index );
 
-            //Accumulo ogni carattere che incontro
-            if( c != '(' ) compactPr += c;
+            //Accumulo ogni carattere che incontro diverso dalle parentesi
+            if( c != '(' && c != '[' ) compactPr += c;
             //Nel caso trovassi un sottogruppo
             else
             {
-                CompactInfo infoSubGroup = saveGroup( index + 1, pr );
+            	if( c == '(' ) addBracket = true;
+            	
+            	CompactInfo infoSubGroup = saveGroup( index + 1, pr, addBracket );
 
                 compactPr += infoSubGroup.indexTable;
                 index = infoSubGroup.indexEndGroup;
@@ -223,7 +239,7 @@ public class Solve implements Runnable
             index++;
         }
 
-        String ref = tableGroup.insertReference( compactPr );
+        String ref = tableGroup.insertReference( compactPr, bracketObligatory );
 
         return new CompactInfo( ref, index );
     }
@@ -251,7 +267,7 @@ public class Solve implements Runnable
     
     private String atomicVariableSet( ArrayList<String> variable, char set )
     {
-        String setting = new String();
+    	String setting = new String();
         int cntVar = variable.size();
 
         //Il risultato e' la stringa composta dalla variabili atomiche
@@ -269,6 +285,8 @@ public class Solve implements Runnable
 
     private String compatta( String pr )
     {
+    	boolean addBracket = false;
+    	
         int index = 0;
         int lenghtPr = pr.length();
 
@@ -279,13 +297,16 @@ public class Solve implements Runnable
             char c = pr.charAt( index );
 
             //Trovato un gruppo di proposizioni
-            if( c == '(' )
+            if( c == '(' || c == '[' )
             {
+            	//Trovata parentesi aggiunta dall'utente
+            	if( c == '(' ) addBracket = true;
+            	
                 //La funzione salvera il gruppo appena trovato nella
                 //ReferenceTable, insieme a tutti i sottogruppi presenti.
                 //Inoltre ci restituisce l'indice che ha usato per identificarla
                 //e la posizione della stringa raggiunto dal gruppo
-                CompactInfo info = saveGroup( index + 1, pr );
+                CompactInfo info = saveGroup( index + 1, pr, addBracket );
 
                 //La nuova stringa avra' l'indice della tabella per
                 //rappresentare il gruppo di proposizioni, semplificando
@@ -305,7 +326,92 @@ public class Solve implements Runnable
         return temp;
     }
 
+    /**
+     * @param phrase : Elemento del sequente contenente parti compresse
+     * @return La stringa equivalente sostituiendo alla parte compresse la stringa allegata estesa
+     */
+    private String decomprimi( String sentence )
+    {
+    	boolean bracketsUseful = true;
+    	
+    	int indexSharp = 0;
+    	
+    	String compressInd = new String();
+    	
+    	while( ( indexSharp = sentence.indexOf( '#' ) ) != -1 )
+    	{
+    		//Nel caso di elementi singolo le parentesi sarebbero solo una notazione superflue
+    		bracketsUseful = ( !sentence.matches( "#.#" ) && !sentence.matches( "#..#" )  );
+    		
+    		//Estrapolo l'elemento compatto della frase
+    		compressInd = sentence.substring( indexSharp, sentence.indexOf( '#', indexSharp + 1 ) + 1 );
+    		//Sostituisco l'elemento compresso rappresentato da compressInd con la stringa estesa contenuta nella tabella dei riferimenti
+    		sentence = sentence.replace( compressInd , tableGroup.getReference( compressInd , bracketsUseful ) );
+    	}
+    	
+    	return sentence;
+    }
+    
 
+    /**
+     * @param sx : parte sinistra del sequente da attaccare all'albero
+     * @param dx : parte destra del sequente da attaccare all'albero
+     * @param idThread : id thread che lavora al sequente
+     * @return Dato il contenuto delle due liste, crea due stringe estendendo ogni sequente compatto ed inserendo le parentesi tonde immesse dall'utente, infine le salva in un oggetto Derivate che viene restituito   
+     */
+    private Derivate createInfoNodo( ArrayList<String> sx, ArrayList<String> dx )
+    {
+    	int dimSx = sx.size();
+    	int dimDx = dx.size() - 1;
+    	
+    	String pre = new String();
+    	
+    	//Decomprimo gli elementi della lista a sinistra
+    	for( int indexSx = 1; indexSx < dimSx; indexSx++ )
+    	{
+    		pre += decomprimi( sx.get( indexSx ) );
+    		if( indexSx < dimSx - 1 ) pre += ',';
+    	}
+    	
+    	pre += " |- ";
+    	
+    	//Decomprimo gli elementi della lista a destra
+    	for( int indexDx = 0; indexDx < dimDx; indexDx++ ) 
+    	{
+    		pre += decomprimi( dx.get( indexDx ) );
+    		if( indexDx < dimDx - 1 ) pre += ',';
+    	}
+    	
+    	return new Derivate( pre );
+    }
+    
+   
+    /**
+     * @param sx : parte sinistra del sequente da derivare
+     * @param dx : parte destra del sequente da derivare
+     * @param idThread : id assegnato al thread che richiede di aggiungere il sequente all'albero
+     * @return Viene attaccato all'albero destro un nuovo nodo contenente il sequente nel formato esteso
+     */
+    private void attachTreeDx( ArrayList<String> sx, ArrayList<String> dx, int idThread )
+    {
+    	Derivate infoNodo = createInfoNodo( sx, dx );
+    	alberoDerivato.addDx( infoNodo, idThread );
+    }
+    
+    
+    /**
+     * @param sx : parte sinistra del sequente da derivare
+     * @param dx : parte destra del sequente da derivare
+     * @param idThread : id assegnato al thread che richiede di aggiungere il sequente all'albero
+     * @return Viene attaccato all'albero sinistro un nuovo nodo contenente il sequente nel formato esteso
+     */
+    private void attachTreeSx( ArrayList<String> sx, ArrayList<String> dx, int idThread )
+    {
+    	Derivate infoNodo = createInfoNodo( sx, dx );
+    	alberoDerivato.addSx( infoNodo, idThread );
+    }
+    
+    
 
     private synchronized void creaRamo( ArrayList<String> sx, ArrayList<String> dx, String s, int verso, int idThreadCalled )
     {
@@ -332,7 +438,7 @@ public class Solve implements Runnable
         
         //Divido l'albero derivato in due rami indipendenti
         alberoDerivato.branchSplit( idThreadCalled, indexSequent );
-        alberoDerivato.addDx( pr, indexSequent );
+        attachTreeDx( pr.prSx, pr.prDx, indexSequent );
     }
 
 
@@ -358,7 +464,7 @@ public class Solve implements Runnable
             {
                 //Decomprimo il periodo e lo inserisco al posto di
                 //quello compatto
-                listSx.add( tableGroup.getReference( listSx.remove( lastElemSx ) ) );
+                listSx.add( tableGroup.getReference( listSx.remove( lastElemSx ), false ) );
             }
 
             //Presente un periodo compatto singolo nella lista di destra
@@ -366,7 +472,7 @@ public class Solve implements Runnable
             {
                 //Decomprimo il periodo e lo inserisco al posto di
                 //quello compatto
-                listDx.add( 0, tableGroup.getReference( listDx.remove( 0 ) ) );
+                listDx.add( 0, tableGroup.getReference( listDx.remove( 0 ), false ) );
             }
 
             //Le regole sono ordinate in ordine crescente al numero di
@@ -385,7 +491,7 @@ public class Solve implements Runnable
                 listSx.add( splitE[0] );
                 listSx.add( splitE[1] );
                 
-                alberoDerivato.addSx( new Predicate( listSx, listDx ), idThread );
+                attachTreeSx( listSx, listDx , idThread );
             }
 
             //Applicata la regola v destra
@@ -401,7 +507,7 @@ public class Solve implements Runnable
                 listDx.add( 0, splitV[0] );
                 listDx.add( 1, splitV[1] );
                 
-                alberoDerivato.addSx( new Predicate( listSx, listDx ), idThread );
+                attachTreeSx( listSx, listDx , idThread );
             }
 
             //Applicata la regola del - sinistra
@@ -416,7 +522,7 @@ public class Solve implements Runnable
                 //La porto nella lista di destra mettendola all'inizio
                 listDx.add( 0, derElem );
                 
-                alberoDerivato.addSx( new Predicate( listSx, listDx ), idThread );
+                attachTreeSx( listSx, listDx , idThread );
             }
 
             //Applicata la regola del - destra
@@ -431,7 +537,7 @@ public class Solve implements Runnable
                 //La porto nella lista di sinistra facendo il push
                 listSx.add( derElem );
                 
-                alberoDerivato.addSx( new Predicate( listSx, listDx ), idThread );
+                attachTreeSx( listSx, listDx , idThread );
             }
 
             //Applicata la regola del > destra
@@ -450,7 +556,7 @@ public class Solve implements Runnable
                 //Il parametro B lo inseriamo all'inizio della lista di destra
                 listDx.add( 0, splitImpl[1] );
                 
-                alberoDerivato.addSx( new Predicate( listSx, listDx ), idThread );
+                attachTreeSx( listSx, listDx , idThread );
             }
 
             //Applicata la regola della & destra
@@ -478,7 +584,7 @@ public class Solve implements Runnable
                 //da creare il ramo sinistro
                 listDx.add( 0, splitE[0] );
                 
-                alberoDerivato.addSx( new Predicate( listSx, listDx ), idThread );
+                attachTreeSx( listSx, listDx , idThread );
             }
 
             //Applicata la regola della v sinistra
@@ -506,7 +612,7 @@ public class Solve implements Runnable
                 //da creare il ramo sinistro
                 listSx.add( splitV[0] );
                 
-                alberoDerivato.addSx( new Predicate( listSx, listDx ), idThread );
+                attachTreeSx( listSx, listDx , idThread );
             }
 
             //Applicata la regola del > sinstra
@@ -534,7 +640,7 @@ public class Solve implements Runnable
                 //di sinistra
                 listDx.add( 0, splitImp[0] );
                 
-                alberoDerivato.addSx( new Predicate( listSx, listDx ), idThread );
+                attachTreeSx( listSx, listDx , idThread );
             }
 
             //Non potendo applicare nessuna regola di derivazione possiamo
@@ -567,7 +673,7 @@ public class Solve implements Runnable
                     	if( listSx.size() < sizeSeqSx )
                     	{
                     		alberoDerivato.ruleUsed( "comp-sx", idThread );
-                    		alberoDerivato.addSx( new Predicate( listSx, listDx ), idThread );
+                    		attachTreeSx( listSx, listDx , idThread );
                     	}
                     	
                     	//Applico la regola del compattamento a destra
@@ -576,13 +682,17 @@ public class Solve implements Runnable
                     	if( listDx.size() < sizeSeqDx )
                     	{
                     		alberoDerivato.ruleUsed( "comp-dx", idThread );
-                    		alberoDerivato.addSx( new Predicate( listSx, listDx ), idThread );
+                    		attachTreeSx( listSx, listDx , idThread );
                     	}
                     	
                     	if( isAxiomIdentity( listSx, listDx ) ) alberoDerivato.ruleUsed( "ax-id", idThread );
                     	//Nella foglia dell'albero, se non e' una assioma, verra scritta nella regola usata i valori delle variabili per cui il nodo e' falso
                     	else
                     	{
+                    		//Elimino i caratteri % e @ dagli ArrayList rispettivamente sinistro e destro essendo superflui
+                    		listSx.remove( 0 );
+                    		listDx.remove( listDx.size() - 1 );
+                    		
                     		String variableSet = atomicVariableSet( listSx, '1' ) + atomicVariableSet( listDx, '0' );
                     		alberoDerivato.ruleUsed( variableSet, idThread );
                     	}
@@ -604,7 +714,7 @@ public class Solve implements Runnable
                         //La setto in posizione 0
                         listDx.set( 0, derElem );
                         
-                        alberoDerivato.addSx( new Predicate( listSx, listDx ), idThread );
+                        attachTreeSx( listSx, listDx , idThread );
                     }
                 }
 
@@ -620,7 +730,7 @@ public class Solve implements Runnable
                     //La setto alla fine della lista
                     listSx.set( lastElemSx, derElem );
                     
-                    alberoDerivato.addSx( new Predicate( listSx, listDx ), idThread );
+                    attachTreeSx( listSx, listDx , idThread );
                 }
             }
         }
